f3d5bc1734d9e16d9366eb22b9cd9c9b117408f6
努力学习C++中de江江好 <469672930@qq.com>
2018-04-24 21:57:34 +0800

    绕过驱动保护
    绕过驱动保护

---
+++ 项目/013_绕过驱动保护(重要_以后hook-NtOpenProcess测试)/mini_ddk/mini_ddk.c
@@ -0,0 +1,126 @@
+//_stdcall
+#include "mini_ddk.h"
+#pragma  INITCODE
+JMPCODE oldCode;	//用来保存前5字节 以便恢复
+PJMPCODE pcur;
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject,PUNICODE_STRING B) //TYPEDEF LONG NTSTATUS
+{  
+	ULONG cur,old;
+	JMPCODE JmpCode;
+	
+	cur=GetNt_CurAddr();//A
+	old=GetNt_OldAddr();//C
+	if (cur!=old){ 	//被hook了
+		pcur=(PJMPCODE)(cur)	//初始化指针
+		//没修改前保存
+		oldCode.E9=pcur->E9;
+		oldCode.JMPADDR=pcur->JMPADDR;	//4字节
+		//
+		
+	    JmpCode.E9=0xE9; 
+		JmpCode.JMPADDR=old-cur-5;
+	    KdPrint(("要写入的地址%X",JmpCode.JMPADDR));
+		
+        //写入JMP   C-A-5=B //实际要写入地址
+		__asm//去掉页面保护
+		{
+			cli
+			mov eax,cr0
+			and eax,not 10000h //and eax,0FFFEFFFFh
+			mov cr0,eax
+
+		}
+		
+		pcur->E9=0xE9;//jmp
+		pcur->JMPADDR=JmpCode.JMPADDR;//要跳转的地址
+
+		 __asm //恢复页保护
+		 {
+			mov eax,cr0
+			or  eax,10000h //or eax,not 0FFFEFFFFh
+			mov cr0,eax
+			sti
+		 }
+		
+		KdPrint(("NtOpenProcess被HOOK了"));
+	}
+ /* ULONG SSDT_NtOpenProcess_Cur_Addr;
+KdPrint(("驱动成功被加载...OK++++++++\n\n"));
+ //读取SSDT表中 NtOpenProcess当前地址 KeServiceDescriptorTable
+ // [[KeServiceDescriptorTable]+0x7A*4] 
+
+__asm
+{    int 3
+	push ebx
+	push eax
+		mov ebx,KeServiceDescriptorTable
+		mov ebx,[ebx] //表的基地址
+		mov eax,0x7a
+		shl eax,2//0x7A*4 //imul eax,eax,4//shl eax,2
+		add ebx,eax//[KeServiceDescriptorTable]+0x7A*4
+		mov ebx,[ebx]
+        mov SSDT_NtOpenProcess_Cur_Addr,ebx
+	pop  eax	
+	pop  ebx
+}
+KdPrint(("SSDT_NtOpenProcess_Cur_Addr=%x\n\n",SSDT_NtOpenProcess_Cur_Addr));*/
+ //注册派遣函数
+pDriverObject->MajorFunction[IRP_MJ_CREATE]=ddk_DispatchRoutine_CONTROL; //IRP_MJ_CREATE相关IRP处理函数
+pDriverObject->MajorFunction[IRP_MJ_CLOSE]=ddk_DispatchRoutine_CONTROL; //IRP_MJ_CREATE相关IRP处理函数
+pDriverObject->MajorFunction[IRP_MJ_READ]=ddk_DispatchRoutine_CONTROL; //IRP_MJ_CREATE相关IRP处理函数
+pDriverObject->MajorFunction[IRP_MJ_CLOSE]=ddk_DispatchRoutine_CONTROL; //IRP_MJ_CREATE相关IRP处理函数
+pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL]=ddk_DispatchRoutine_CONTROL; //IRP_MJ_CREATE相关IRP处理函数
+ CreateMyDevice(pDriverObject);//创建相应的设备
+ pDriverObject->DriverUnload=DDK_Unload;
+return (1);
+}
+//#pragma code_seg("PAGE")
+#pragma PAGECODE
+VOID DDK_Unload (IN PDRIVER_OBJECT pDriverObject)
+{
+  PDEVICE_OBJECT pDev;//用来取得要删除设备对象
+  UNICODE_STRING symLinkName; // 
+  
+		__asm//去掉页面保护
+		{
+			cli
+			mov eax,cr0
+			and eax,not 10000h //and eax,0FFFEFFFFh
+			mov cr0,eax
+		}
+		
+		//pcur=(PJMPCODE)(cur)
+		pcur->E9 = oldCode.E9;
+		pcur->JMPADDR = oldCode.JMPADDR;	//4字节
+
+		 __asm //恢复页保护
+		 {
+			mov eax,cr0
+			or  eax,10000h //or eax,not 0FFFEFFFFh
+			mov cr0,eax
+			sti
+		 }
+ 
+  pDev=pDriverObject->DeviceObject;
+  IoDeleteDevice(pDev); //删除设备
+  
+  //取符号链接名字
+   RtlInitUnicodeString(&symLinkName,L"\\??\\yjx888");
+  //删除符号链接
+   IoDeleteSymbolicLink(&symLinkName);
+ KdPrint(("驱动成功被卸载...OK-----------")); //sprintf,printf
+ //取得要删除设备对象
+//删掉所有设备
+ DbgPrint("卸载成功");
+}
+#pragma PAGECODE
+NTSTATUS ddk_DispatchRoutine_CONTROL(IN PDEVICE_OBJECT pDevobj,IN PIRP pIrp	)
+{
+	//对相应的IPR进行处理
+	pIrp->IoStatus.Information=0;//设置操作的字节数为0，这里无实际意义
+	pIrp->IoStatus.Status=STATUS_SUCCESS;//返回成功
+	IoCompleteRequest(pIrp,IO_NO_INCREMENT);//指示完成此IRP
+	KdPrint(("离开派遣函数\n"));//调试信息
+	return STATUS_SUCCESS; //返回成功
+}
---
+++ 项目/013_绕过驱动保护(重要_以后hook-NtOpenProcess测试)/mini_ddk/mini_ddk.h
@@ -0,0 +1,100 @@
+#include <ntddk.h>
+#include <windef.h>
+#define INITCODE code_seg("INIT") 
+#define PAGECODE code_seg("PAGE") /*表示内存不足时，可以被置换到硬盘*/
+typedef struct _ServiceDescriptorTable {
+	PVOID ServiceTableBase; //System Service Dispatch Table 的基地址  
+	PVOID ServiceCounterTable;
+	//包含着 SSDT 中每个服务被调用次数的计数器。这个计数器一般由sysenter 更新。 
+	unsigned int NumberOfServices;//由 ServiceTableBase 描述的服务的数目。  
+	PVOID ParamTableBase; //包含每个系统服务参数字节数表的基地址-系统服务参数表 
+}*PServiceDescriptorTable;  
+extern PServiceDescriptorTable KeServiceDescriptorTable;
+
+#pragma pack(1)
+typedef struct _JMPCODE
+{
+	BYTE E9;
+	ULONG JMPADDR;//88881234=B
+}JMPCODE,*PJMPCODE;
+#pragma pack()
+
+#pragma INITCODE /*指的代码运行后 就从内存释放掉*/
+NTSTATUS CreateMyDevice (IN PDRIVER_OBJECT pDriverObject) 
+{
+	NTSTATUS status;
+	PDEVICE_OBJECT pDevObj;/*用来返回创建设备*/
+
+	//创建设备名称
+	UNICODE_STRING devName;
+	UNICODE_STRING symLinkName; // 
+	RtlInitUnicodeString(&devName,L"\\Device\\yjxDDK_Device");/*对devName初始化字串为 "\\Device\\yjxDDK_Device"*/
+
+	//创建设备
+	status = IoCreateDevice( pDriverObject,\
+		0,\
+		&devName,\
+		FILE_DEVICE_UNKNOWN,\
+		0, TRUE,\
+		&pDevObj);
+	if (!NT_SUCCESS(status))
+	{
+		if (status==STATUS_INSUFFICIENT_RESOURCES)
+		{
+			KdPrint(("资源不足 STATUS_INSUFFICIENT_RESOURCES"));
+		}
+		if (status==STATUS_OBJECT_NAME_EXISTS )
+		{
+			KdPrint(("指定对象名存在"));
+		}
+		if (status==STATUS_OBJECT_NAME_COLLISION)
+		{
+			KdPrint(("//对象名有冲突"));
+		}
+		KdPrint(("设备创建失败...++++++++"));
+		return status;
+	}
+	KdPrint(("设备创建成功...++++++++"));
+
+	pDevObj->Flags |= DO_BUFFERED_IO;
+	//创建符号链接
+
+	RtlInitUnicodeString(&symLinkName,L"\\??\\yjx888");
+	status = IoCreateSymbolicLink( &symLinkName,&devName );
+	if (!NT_SUCCESS(status)) /*status等于0*/
+	{
+		IoDeleteDevice( pDevObj );
+		return status;
+	}
+	return STATUS_SUCCESS;
+}
+
+VOID DDK_Unload (IN PDRIVER_OBJECT pDriverObject); //前置说明 卸载例程
+NTSTATUS ddk_DispatchRoutine_CONTROL(IN PDEVICE_OBJECT pDevobj,IN PIRP pIrp	);//派遣函数
+
+ULONG GetNt_CurAddr() //获取当前SSDT_NtOpenProcess的当前地址
+{
+
+LONG *SSDT_Adr,SSDT_NtOpenProcess_Cur_Addr,t_addr; 
+KdPrint(("驱动成功被加载中.............................\n"));
+//读取SSDT表中索引值为0x7A的函数
+//poi(poi(KeServiceDescriptorTable)+0x7a*4)
+t_addr=(LONG)KeServiceDescriptorTable->ServiceTableBase;
+KdPrint(("当前ServiceTableBase地址为%x \n",t_addr));
+SSDT_Adr=(PLONG)(t_addr+0x7A*4);
+KdPrint(("当前t_addr+0x7A*4=%x \n",SSDT_Adr)); 
+SSDT_NtOpenProcess_Cur_Addr=*SSDT_Adr;	
+KdPrint(("当前SSDT_NtOpenProcess_Cur_Addr地址为%x \n",SSDT_NtOpenProcess_Cur_Addr));
+return SSDT_NtOpenProcess_Cur_Addr;
+}
+
+ULONG GetNt_OldAddr()
+{
+	UNICODE_STRING Old_NtOpenProcess;
+    ULONG Old_Addr;
+	RtlInitUnicodeString(&Old_NtOpenProcess,L"NtOpenProcess");
+	Old_Addr=(ULONG)MmGetSystemRoutineAddress(&Old_NtOpenProcess);//取得NtOpenProcess的地址
+	KdPrint(("取得原函数NtOpenProcess的值为 %x",Old_Addr));
+	return Old_Addr;
+
+}
